8f92396bfb6c03e3c47aa2af9e142aa8e8808682
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-12-10 21:07:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+difference=0;
+rightWristX=0;
+leftWristeX=0;
+
+function setup()
+{
+  video=createCapture(VIDEO);
+  video.size(550,500);
+
+  canvas=createCanvas(550,550);
+  canvas.position(560,160);
+
+  poseNet=ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    background('#a6a6a6');
+    document.getElementById("font-size").innerHTML="font size of text will be ="+difference+"px";
+    textSize(difference);
+    fill('#FF0000');
+    text('SIRI!!!',50,400);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.legnth >0)
+    {
+        console.log (results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + leftWristX + " difference = " +difference);
+    }
+}
+
